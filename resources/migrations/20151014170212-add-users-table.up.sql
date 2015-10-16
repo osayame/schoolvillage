fc09311f310e41291b6dc699012e4287b4c6da29
Osayame Gaius-Obaseki <osayame@gmail.com>
2015-10-15 20:37:06 -0400

    Added DB migration

--- resources/migrations/20151014170212-add-users-table.up.sql
+++ resources/migrations/20151014170212-add-users-table.up.sql
@@ -1,1 +1,56 @@
 
+-- Table: users
+
+-- DROP TABLE users;
+
+CREATE TABLE users
+(
+  id integer NOT NULL,
+  first_name text,
+  email text NOT NULL,
+  phone text,
+  flag_comment text,
+  status company_status,
+  updated_at timestamp with time zone,
+  last_name text,
+  created_at timestamp with time zone,
+  zip text,
+  address1 text,
+  address2 text,
+  city text,
+  country text,
+  biography text,
+  state text,
+  resume text,
+  photo text,
+  url text,
+  linkedin text,
+  CONSTRAINT users_pkey PRIMARY KEY (id),
+  CONSTRAINT unique_col UNIQUE (email, url)
+)
+WITH (
+  OIDS=FALSE
+);
+
+--;;
+
+-- Index: company_name
+
+-- DROP INDEX company_name;
+
+CREATE INDEX first_name
+  ON users
+  USING btree
+  (first_name COLLATE pg_catalog."default");
+
+--;;
+
+-- Index: id
+
+-- DROP INDEX id;
+
+CREATE UNIQUE INDEX id
+  ON users
+  USING btree
+  (id);
+
